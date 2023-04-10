b11c69687de04944bff5754690c597e24a1696ef
Ricardo-Lucena-Clear <126709276+Ricardo-Lucena-Clear@users.noreply.github.com>
2023-04-10 12:47:37 -0300

    React Router Dom v6

--- src/components/layout/Content.jsx
+++ src/components/layout/Content.jsx
@@ -1,10 +1,23 @@
 import "./Content.css";
 import React from "react";
-
+import { Routes, Route } from "react-router-dom";
+ 
+import Home from "../../views/examples/Home";
+import Param from "../../views/examples/Param";
+import About from "../../views/examples/About";
+import NotFound from "../../views/examples/NotFound";
+ 
 const Content = props => (
-    <main className="Content">
-       <h1>Component Content</h1>
-    </main>
-)
-
-export default Content
+  <main className="Content">
+    
+    <Routes>
+      <Route path="/about" element={<About />} />
+      <Route path="/param/:id" element={<Param />} />
+      <Route path="/" exact element={<Home />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+   
+  </main>
+);
+ 
+export default Content;
